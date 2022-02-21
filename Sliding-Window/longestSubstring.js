ea8d536dd545ba4c729d0691396ecdee076bd02a
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-02-21 15:39:53 -0500

    added example for longest substring without repeating characters

--- Sliding-Window/longestSubstring.js
+++ Sliding-Window/longestSubstring.js
@@ -9,18 +9,34 @@ var lengthOfLongestSubstring = function(s) {
     let right = 0;
     let maxString = 0;
     
-    while (right < s.length){        //while right pointer is inside the string length
+    while (right < s.length){        //while right pointer is inside the string
         if (!set.has(s[right])){     //if right pointer's number is not in the set, add it 
             set.add(s[right]);      
 
             maxString = Math.max(maxString, set.size);    //maxString = max number between current set size and maxString
             right++;                //expand window to the right
         } else {                    //if the current number is already in the set...
-            set.delete(s[left]);    //delete left nuberm from the set since it's the first num that is the same
+            set.delete(s[left]);    //delete left number from the set since it's the same as the current number on the right
             left++;                 //decrease the window on the left side
         }
     }
     return max;
 };
 
-//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/*
+
+input = A   B   C   A
+        |--->|
+        |------->|
+        |----------->|
+            |------->|
+
+set = { A             }
+      { A   B         }
+      { A   B   C     }
+      {     B   C   A }
+
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+*/
