783cfb0b6ccb75fa44863ce1d878d28e820ea560
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-02-21 17:17:03 -0500

    added valid palindrome

--- Two-Pointers/ReverseString.js
+++ Two-Pointers/ReverseString.js
@@ -1,3 +1,8 @@
+//Reverse String (EASY)
+
+//Prompt: Write a function that reverses a string. The input string is given as an array of characters s.
+//You must do this by modifying the input array in-place with O(1) extra memory.
+
 var reverseString = function(s) {
     
     let left = 0;
---
+++ Two-Pointers/ValidPalindrome.js
@@ -0,0 +1,47 @@
+//Valid Palindrome (EASY)
+
+//Prompt: Given a string s, return true if it is a palindrome, or false otherwise.
+
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (!isValidCharacter(s[left])) {
+      //if left character is not valid, move left pointer up
+      left++;
+    }
+
+    while (!isValidCharacter(s[right])) {
+      //if right character is not valid, move right pointer down
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      //if both characters are valid but they dont equal each other, return false
+      return false;
+    }
+
+    left++; //if both characters are valid and they do equal to each other, move both pointers
+    right--;
+  }
+  return true; //return true at the end as both pointers are now pointing to the same place, meaning the whole string is valid
+};
+
+let isValidCharacter = function (char) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"; // valid characters are only letters and numbers, no special chaaracters or spaces
+
+  return alphabet.indexOf(char.toLowerCase()) > -1; //if character is valid, return the lowercase version of it
+};
+
+//indexOf() returns -1 if the character is not in the string
+
+/*
+           L             R
+             L         R
+               L     R
+                 L R            "a" and "e" aren't the same letter, return false and exit function
+Input =  " r a c e a c a r "
+
+https://leetcode.com/problems/valid-palindrome/
+*/
