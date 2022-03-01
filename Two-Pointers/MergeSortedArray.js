0943c9cdf20638b273e950abde8f5be7047cf90a
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-03-01 10:36:15 -0500

    switched to Blind 75

--- Two-Pointers/MergeSortedArray.js
+++ Two-Pointers/MergeSortedArray.js
@@ -1,6 +1,7 @@
 //Merge Sorted Array (EASY)
 
-//Prompt: Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//Prompt: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 
 var merge = function(nums1, m, nums2, n) {
